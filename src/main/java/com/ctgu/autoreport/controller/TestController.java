f9f649e05b1a9d94a7c545e058bda2fa1bdd7db3
Elm Forest <timbrightland@gmail.com>
2022-10-18 20:31:43 +0800

    修复Bug

--- src/main/java/com/ctgu/autoreport/controller/TestController.java
+++ src/main/java/com/ctgu/autoreport/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ctgu.autoreport.controller;
 
+import com.ctgu.autoreport.common.utils.AesUtils;
 import com.ctgu.autoreport.service.core.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,17 @@ public class TestController {
     @Autowired
     private ReportService report;
 
+    @Autowired
+    private AesUtils aesUtils;
+
     @RequestMapping("/test")
     public String test() {
         report.report();
         return "reporting~";
+    }
+
+    @RequestMapping("/api/pwd")
+    public String getPwd(String pwd) {
+        return aesUtils.decryptAes(pwd);
     }
 }
