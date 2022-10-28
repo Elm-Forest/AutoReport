9720647099208069de6d7f9e9d629c913cdf0909
Elm Forest <timbrightland@gmail.com>
2022-10-28 16:29:38 +0800

    更新cron表达式

--- src/main/java/com/ctgu/autoreport/AutoReportApplication.java
+++ src/main/java/com/ctgu/autoreport/AutoReportApplication.java
@@ -2,15 +2,12 @@ package com.ctgu.autoreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Elm Forest
  */
-@EnableAsync
-@EnableScheduling
+
 @EnableTransactionManagement
 @SpringBootApplication
 public class AutoReportApplication {
